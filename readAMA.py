40a9a3329d0d757d719003940658c09de0ca3f3a
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-20 12:50:45 +0000

    Added docstring and empty two-point read function.

--- readAMA.py
+++ readAMA.py
@@ -1,3 +1,17 @@
 import numpy as np
 
-def read2pt(exact_filename, sloppy_filename, )
+def read2pt(exact_filename, sloppy_filename, time_extent, num_src)
+    """Reads in the exact and sloppy results from the supplied files
+    and does the AMA
+    
+    :param exact_filename: The file containing the exact results
+    :type exact_filename: :class:`str`
+    :param sloppy_filename: The file containing the sloppy results
+    :type sloppy_filename: :class:`str`
+    :param time_extent: The correlator time extent
+    :type time_extent: :class:`int`
+    :param num_src: The number of sources
+    :type num_src: :class:`int`
+    """
+    
+    pass
