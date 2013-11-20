cf3f2492c145112270c2e2d444014b3364ff4d4a
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-20 15:07:40 +0000

    Got a basic python version working. Slower than C but hopefully can improve that

--- readAMA.py
+++ readAMA.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def read2pt(exact_filename, sloppy_filename, time_extent, num_src)
+def read2pt(exact_filename, sloppy_filename, time_extent, num_src):
     """Reads in the exact and sloppy results from the supplied files
     and does the AMA
     
@@ -14,4 +14,41 @@ def read2pt(exact_filename, sloppy_filename, time_extent, num_src)
     :type num_src: :class:`int`
     """
     
-    pass
+    exact_raw_data = np.genfromtxt(exact_filename)
+    # Convert the last two columns into a complex value
+    # We reshape so the first index in the array corresponds to the source
+    exact_correlators \
+      = np.reshape(exact_raw_data[:, 2] + 1j * exact_raw_data[:, 3],
+                   (num_src, time_extent))
+    
+    # Move every num_srcth datum to a list of sources file
+    exact_t_src = np.int64(exact_raw_data[::time_extent, 0])
+    
+    # Check that there are time_extent * num_src data
+    if exact_correlators.size != time_extent * num_src:
+        raise IOError("Expected {} rows in exact data file, found {}"
+                      .format(time_extent * num_src, exact_correlators.size))
+    
+    # Here we reshape the correlator into a 2d array of shape
+    # num_src x time_extent and then average over the sources
+    # (axis zero of the new array)
+    exact_source_average = np.mean(exact_correlators, axis=0)
+    
+    # Now read in the sloppies
+    sloppy_raw_data = np.genfromtxt(sloppy_filename)
+    sloppy_correlators \
+      = np.reshape(sloppy_raw_data[:, 2] + 1j * sloppy_raw_data[:, 3],
+                   (time_extent, time_extent))
+    
+    sloppy_source_average = np.mean(sloppy_correlators, axis=0)
+    
+    # Subtract relevant sloppy correlators from corresponding exact correlators
+    for i, src in enumerate(exact_t_src):
+        exact_correlators[i] -= sloppy_correlators[src]
+        
+    residual_source_average = np.mean(exact_correlators, axis=0)
+    
+    ama_source_average = residual_source_average + sloppy_source_average
+    
+    return exact_source_average, sloppy_source_average, \
+      residual_source_average, ama_source_average
