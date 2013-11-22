6b4787bfec005add787cf7944ed29437f19131ba
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-22 22:14:53 +0000

    Removed deprecated reference to boost python library.

--- etaetaprime/fileio.py
+++ etaetaprime/fileio.py
@@ -1,8 +1,6 @@
 # Use cython to speed up the conversion to a numpy array from a list
 import pyximport
 pyximport.install()
-# Comment out the following line if not using the boost compiled file reader
-#from fastfunctions import fastread
 from fastfunctions import converters
 
 def file_to_list(filename):
@@ -26,8 +24,5 @@ def load_data(filename):
     :type filename: :class:`str`
     :returns: :class:`numpy.ndarray`
     """
-    
-    # Could use fastread.read_csv here instead of _file_to_list, but at the
-    # moment this affords no performance increase
-    list_read_function = _file_to_list
-    return converters.toarr(list_read_function(filename))
+
+    return converters.toarr(file_to_list(filename))
