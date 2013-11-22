e22ee2668f0a9c3189c92a95a7a41ac99c953f8d
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-22 15:22:31 +0000

    Added correlators module to calculate correlators from traces.

---
+++ etaetaprime/correlators.py
@@ -0,0 +1,28 @@
+import ama
+import numpy as np
+import fileio
+
+def combine_traces(first_trace, second_trace, first_timeslices=None,
+                   second_timeslices=None):
+    """Computes a correlator from a pair of traces and
+    their corresponding timeslices
+    
+    :param first_trace: The first set of traces
+    :type first_trace: :class:`numpy.ndarray`
+    :param second_trace: The second set of traces
+    :type second_trace: :class:`numpy.ndarray`
+    :param first_timeslices: The set of timeslices over which the
+    first traces are taken
+    :type first_timeslices: :class:`numpy.ndarray` or :class:`list`
+    :param second_timeslices: The set of timeslices over which the
+    second traces are taken
+    :type first_timeslices: :class:`numpy.ndarray` or :class:`list`
+    """
+    
+    # If no timeslices are given, assume the form they take
+    if first_timeslices == None:
+        first_timeslices = np.arange(first_trace.size)
+    if second_timeslices == None:
+        second_timeslices = np.arange(second_trace.size)
+        
+    
