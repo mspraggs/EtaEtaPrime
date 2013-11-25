5fdf7d82c70165eb825c80fc03dbd78fceeb066e
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-25 10:38:23 +0000

    Added some comments to the combine_traces function.

--- etaetaprime/correlators.py
+++ etaetaprime/correlators.py
@@ -29,12 +29,22 @@ def combine_traces(first_trace, second_trace, first_timeslices=None,
     if second_timeslices == None:
         second_timeslices = np.arange(second_trace.size)
         
+    # Make sure the supplied timeslices are in int64 format
+    first_timeslices = np.int64(first_timeslices)
+    second_timeslices = np.int64(second_timeslices)
+    
+    # Get all possible differences between the various timeslice pairings
+    # (This works like an outer product, returning a 2d array)
     diffs = combinatorics.diff(first_timeslices, second_timeslices)
+    # Get the products of all the correlators
     prods = np.outer(first_trace, second_trace)
+    # Get the output timeslices by determining the uniqe list of diffs
     timeslices = np.unique(diffs)
+    # The output array
     correlator = np.zeros(timeslices.size, dtype=np.complex)
     
+    # Now loop over the unique timeslices and sum the corresponding products
     for i, t in enumerate(timeslices):
         correlator[i] = np.sum(prods[diffs == t])
-        
+    # Turn the result into a numpy array
     return np.array([timeslices, correlator])
