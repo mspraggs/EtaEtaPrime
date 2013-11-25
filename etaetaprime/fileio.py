feaa269324336e598eba45c74a5070ba7500bd1e
Matt Spraggs <matthew.spraggs@gmail.com>
2013-11-25 10:46:03 +0000

    Added specific load correlators and load_traces functions to fileio

--- etaetaprime/fileio.py
+++ etaetaprime/fileio.py
@@ -26,3 +26,36 @@ def load_data(filename):
     """
 
     return converters.toarr(file_to_list(filename))
+
+def load_correlators(filename):
+    """Loads the connected two point function in the specified file
+    
+    :param filename: The file from which to load the correlators
+    :type filename: :class:`str`
+    :returns: :class:`numpy.ndarray`
+    """
+    
+    raw_data = load_data(filename)
+    out = np.zeros((raw_data.shape[0], 3), dtype=np.complex)
+    
+    out[:, 0:2] = raw_data[:, 0:2]
+    out[:, 2] = raw_data[:, 2] + 1j * raw_data[:, 3]
+    
+    return out
+
+def load_traces(filename):
+    """Loads the two traces in the specified file
+    
+    :param filename: The file from which to load the traces
+    :type filename: :class:`str`
+    :returns: :class:`numpy.ndarray`
+    """
+    
+    raw_data = load_data(filename)
+    out = np.zeros((raw_data.shape[0], 4), dtype=np.complex)
+    
+    out[:, 0] = raw_data[:, 0]
+    out[:, 1] = raw_data[:, 1] + 1j * raw_data[:, 2]
+    out[:, 2] = raw_data[:, 3] + 1j * raw_data[:, 4]
+
+    return out
